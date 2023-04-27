9bd41bc9953602d61a4b8d043b2a8d65cca04777
masonsohn23 <masonsohn23@gmail.com>
2023-04-27 09:45:42 -0700

    adapter

---
+++ app/src/main/java/com/example/ratemymovie/MovieAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.ratemymovie
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MovieAdapter(var dataset: List<MovieData>):
+RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+    class ViewHolder (view: View): RecyclerView.ViewHolder(view){
+        val
+    }
+}
