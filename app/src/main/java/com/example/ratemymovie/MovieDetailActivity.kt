2ac9103e33b6e0ad5b68d2abbd057618f26b4633
masonsohn23 <masonsohn23@gmail.com>
2023-05-11 09:22:20 -0700

    rating

--- app/src/main/java/com/example/ratemymovie/MovieDetailActivity.kt
+++ app/src/main/java/com/example/ratemymovie/MovieDetailActivity.kt
@@ -16,8 +16,10 @@ class MovieDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMovieDetailBinding
     var MovieIsEditable = false
     lateinit var movie: MovieData
+    lateinit var rating: Rating
     companion object {
         val EXTRA_MOVIE = "movie"
+        val EXTRA_RATING = "rating"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +27,14 @@ class MovieDetailActivity : AppCompatActivity() {
         binding = ActivityMovieDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         var passedMovie = intent.getParcelableExtra<MovieData>(EXTRA_MOVIE)
+        var passedRating = intent.getParcelableExtra<Rating>(EXTRA_RATING)
 
+        if(passedRating == null){
+            rating = Rating()
+            toggleEditable()
+        } else {
+            binding.ratingBarDetailRating.rating = (passedRating.rating)
+        }
         if (passedMovie == null) {
             movie = MovieData()
             toggleEditable()
@@ -39,12 +48,13 @@ class MovieDetailActivity : AppCompatActivity() {
             binding.textViewDetailYear.setText(passedMovie.year).toString()
         }
             binding.buttonDetailSave.setOnClickListener {
-                movie.rating = binding.ratingBarDetailRating.rating
-                movie.name = binding.textViewDetailName.text.toString()
+                if(rating.ownerId.isNullOrBlank()){
+                    rating.ownerId = intent.getStringExtra(MovieListActivity.EXTRA_USER_ID)!!
+                }
 
-                Backendless.Data.of(MovieData::class.java)
-                    .save(movie, object : AsyncCallback<MovieData> {
-                        override fun handleResponse(response: MovieData?) {
+                Backendless.Data.of(Rating::class.java)
+                    .save(rating, object : AsyncCallback<Rating> {
+                        override fun handleResponse(response: Rating?) {
                             Log.d("hand", "handleResponse ${response}")
                         }
 
--- app/src/main/java/com/example/ratemymovie/Rating.kt
+++ app/src/main/java/com/example/ratemymovie/Rating.kt
@@ -6,5 +6,7 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Rating (
     var imbdbID: String ="tt2015381",
-var rating: Float = 4.5f
+    var rating: Float = 4.5f,
+    var ownerId: String? = null,
+    var objectID: String? = null
         ):Parcelable
