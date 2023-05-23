3a2387f24743531b6567e9b1d215d8d65ae5b579
= <=>
2023-05-23 09:51:46 -0700

    menu

--- app/src/main/java/com/example/ratemymovie/MovieListActivity.kt
+++ app/src/main/java/com/example/ratemymovie/MovieListActivity.kt
@@ -8,6 +8,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.backendless.Backendless
 import com.example.ratemymovie.databinding.ActivityMovieListBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -65,20 +66,31 @@ class MovieListActivity : AppCompatActivity() {
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.menu_loan_detail, menu)
+        inflater.inflate(R.menu.menu_login, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId){
-            R.id.menu_login ->{
+            R.id.menu_log_reg ->{
                 val loginIntent = Intent(this, LoginActivity :: class.java)
                 startActivity(loginIntent)
                 true
             }
-            R.id.menu_Registration ->{
-                val registrationIntent = Intent(this, RegistrationActivity:: class.java)
-                startActivity(registrationIntent)
+            R.id.my_list ->{
+                if(Backendless.UserService.CurrentUser() == null)
+                {
+                    val loginIntent = Intent(this, LoginActivity :: class.java)
+                    startActivity(loginIntent)
+                }
+                true
+            }
+            R.id.wanted_watch ->{
+                if(Backendless.UserService.CurrentUser() == null)
+                {
+                    val loginIntent = Intent(this, LoginActivity :: class.java)
+                    startActivity(loginIntent)
+                }
                 true
             }
             else -> super.onOptionsItemSelected(item)
