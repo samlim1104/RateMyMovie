6cb86db7acbd92cb497d00515bb2dd855b3db69d
masonsohn23 <masonsohn23@gmail.com>
2023-05-23 08:35:17 -0700

    final rating fixed

--- app/src/main/java/com/example/ratemymovie/MovieAdapter.kt
+++ app/src/main/java/com/example/ratemymovie/MovieAdapter.kt
@@ -1,18 +1,13 @@
 package com.example.ratemymovie
 
-import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
-import com.backendless.Backendless
-import com.backendless.async.callback.AsyncCallback
-import com.backendless.exceptions.BackendlessFault
+
 class MovieAdapter(var movieList: MutableList<MovieData>):
     RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -22,7 +17,7 @@ class MovieAdapter(var movieList: MutableList<MovieData>):
 
         init {
             textViewMovieName = view.findViewById(R.id.textView_movie_name)
-            textViewRating = view.findViewById(R.id.textView_movie_rating)
+            textViewRating = view.findViewById(R.id.textView_movie_matrating)
             layout = view.findViewById(R.id.layout_movie)
 
         }
