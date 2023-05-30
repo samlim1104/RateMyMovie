fdbf9c7f10460093f84e5e4952dbad4e0f544689
masonsohn23 <masonsohn23@gmail.com>
2023-05-30 08:45:04 -0700

    final rating fixed

--- app/src/main/java/com/example/ratemymovie/MovieDetailActivity.kt
+++ app/src/main/java/com/example/ratemymovie/MovieDetailActivity.kt
@@ -65,7 +65,7 @@ class MovieDetailActivity : AppCompatActivity() {
                     binding.textView12DetailMin.text = response.body()?.runtime
                     binding.textViewDetailImdbid.text = response.body()?.objectId
                     binding.textViewDetailYear.text = response.body()?.year.toString()
-                    binding.ratingBarDetailRating.rating = rating.rating
+                    //binding.ratingBarDetailRating.rating = response.body()?.rating!!
 
 
                 }
